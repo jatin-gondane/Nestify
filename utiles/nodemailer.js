c516e029d397112fb0f44816018363db53dab561
Jatin Gondane <jatingondane@gmail.com>
2026-01-28 03:48:03 +0530

    fixing nodemaler bug by changing port

--- utiles/nodemailer.js
+++ utiles/nodemailer.js
@@ -6,8 +6,8 @@ Dotenv.config()
 // For production, replace with your actual SMTP server details.
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true, // Use true for port 465, false for port 587
+  port: 587,
+  secure: false, // Use true for port 465, false for port 587
   auth: {
     user: process.env.nodemailer_mail ,
     pass: process.env.nodemailer_mailKey,
